c0399ffde52677e22c9480373c20e987f62ed26a
tarequr <tarequrr8@gmail.com>
2023-06-25 22:42:00 +0600

    catch all value

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const search = () => {
+    const searchBox   = document.getElementById("search-product").value.toUpperCase();
+    const storeItems  = document.getElementById("product-list");
+    const product     = document.querySelectorAll(".product");
+    const productName = storeItems.getElementsByTagName("h2");
+
+    for (let i = 0; i < productName.length; i++) {
+        let
+        
+    }
+}
